99c18be2552dcec51a3916fe11d1236deedb796f
jay311311 <jay311311@gmail.com>
2020-08-15 23:44:57 +0900

    structuring Poster Compoenet. part1

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
-
+import Poster from "../../Components/Poster";
 const Contanier = styled.div`
 padding : 0px 20px;
 `;
@@ -14,12 +14,24 @@ const HomePresenter  = ({nowPlaying, upcoming, popular, error, loading}) => load
     <Contanier> 
         {/*{nowPlaying.map()}이 에러가 나오는 이유는 loading 할때 nowPlaying은 존재하지 않아서*/ }
         {nowPlaying && nowPlaying.length > 0  && (
-        <Section title="Now Playing">{nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>)}
+        <Section title="Now Playing">
+            {nowPlaying.map(movie => (
+                <Poster/>
+                ))}
+        </Section>)}
         {/*nowPlaying존재하는 지 확인 && nowPlaying길이가 0보다 큰지 확인 && section이 render되는지 확인*/ }
         {upcoming && upcoming.length > 0  && (
-        <Section title="upcoming Movies">{upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>)}
+        <Section title="upcoming Movies">
+            {upcoming.map(movie => (
+                <Poster/>
+            ))}
+        </Section>)}
         {popular && popular.length > 0  && (
-        <Section title="Popular Movies">{popular.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>)}
+        <Section title="Popular Movies">
+            {popular.map(movie => (
+                <Poster/>
+            ))}
+        </Section>)}
         {/*section안에 children을 넣는 이유는 section에서 div 내부에 원하는 children을 넣을수 있어야 해서 */}
         {error && <Message color="#e74c3c" text={error}/>}
     </Contanier>)
